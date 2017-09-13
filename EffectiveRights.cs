a3e2abd98049eb2a74a594b2a422fd87ed1b0540
Mariusz S. Jurgielewicz <mordor@mail.com>
2017-09-12 22:08:25 -0700

    Initial

---
+++ EffectiveRights.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class EffectiveRights
+{
+    public const int ERROR_FILE_NOT_FOUND = 0x2;
+
+    [Flags]
+    public enum SECURITY_INFORMATION : uint
+    {
+        OWNER_SECURITY_INFORMATION = 0x00000001,
+        GROUP_SECURITY_INFORMATION = 0x00000002,
+        DACL_SECURITY_INFORMATION = 0x00000004,
+        SACL_SECURITY_INFORMATION = 0x00000008,
+        UNPROTECTED_SACL_SECURITY_INFORMATION = 0x10000000,
+        UNPROTECTED_DACL_SECURITY_INFORMATION = 0x20000000,
+        PROTECTED_SACL_SECURITY_INFORMATION = 0x40000000,
+        PROTECTED_DACL_SECURITY_INFORMATION = 0x80000000
+    }
+
+    public enum SE_OBJECT_TYPE
+    {
+        SE_UNKNOWN_OBJECT_TYPE = 0,
+        SE_FILE_OBJECT,
+        SE_SERVICE,
+        SE_PRINTER,
+        SE_REGISTRY_KEY,
+        SE_LMSHARE,
+        SE_KERNEL_OBJECT,
+        SE_WINDOW_OBJECT,
+        SE_DS_OBJECT,
+        SE_DS_OBJECT_ALL,
+        SE_PROVIDER_DEFINED_OBJECT,
+        SE_WMIGUID_OBJECT,
+        SE_REGISTRY_WOW64_32KEY
+    }
+
+    public enum MULTIPLE_TRUSTEE_OPERATION
+    {
+        NO_MULTIPLE_TRUSTEE,
+        TRUSTEE_IS_IMPERSONATE
+    }
+
+    public enum TRUSTEE_FORM
+    {
+        TRUSTEE_IS_SID,
+        TRUSTEE_IS_NAME,
+        TRUSTEE_BAD_FORM,
+        TRUSTEE_IS_OBJECTS_AND_SID,
+        TRUSTEE_IS_OBJECTS_AND_NAME
+    }
+
+    public enum TRUSTEE_TYPE
+    {
+        TRUSTEE_IS_UNKNOWN,
+        TRUSTEE_IS_USER,
+        TRUSTEE_IS_GROUP,
+        TRUSTEE_IS_DOMAIN,
+        TRUSTEE_IS_ALIAS,
+        TRUSTEE_IS_WELL_KNOWN_GROUP,
+        TRUSTEE_IS_DELETED,
+        TRUSTEE_IS_INVALID,
+        TRUSTEE_IS_COMPUTER
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto, Pack = 4)]
+    public struct TRUSTEE
+    {
+        public IntPtr pMultipleTrustee; // must be null
+        public MULTIPLE_TRUSTEE_OPERATION MultipleTrusteeOperation;
+        public int TrusteeForm;
+        public int TrusteeType;
+        public IntPtr ptstrName;
+    }
+
+    public enum SID_NAME_USE
+    {
+        SidTypeUser = 1,
+        SidTypeGroup,
+        SidTypeDomain,
+        SidTypeAlias,
+        SidTypeWellKnownGroup,
+        SidTypeDeletedAccount,
+        SidTypeInvalid,
+        SidTypeUnknown,
+        SidTypeComputer
+    }
+
+    public enum ACCESS_MASK : uint
+    {
+        DELETE = 0x00010000,
+        READ_CONTROL = 0x00020000,
+        WRITE_DAC = 0x00040000,
+        WRITE_OWNER = 0x00080000,
+        SYNCHRONIZE = 0x00100000,
+        STANDARD_RIGHTS_REQUIRED = 0x000f0000,
+        STANDARD_RIGHTS_READ = READ_CONTROL,
+        STANDARD_RIGHTS_WRITE = READ_CONTROL,
+        STANDARD_RIGHTS_EXECUTE = READ_CONTROL,
+        STANDARD_RIGHTS_ALL = 0x001f0000,
+        SPECIFIC_RIGHTS_ALL = 0x0000ffff,
+        ACCESS_SYSTEM_SECURITY = 0x01000000,
+        MAXIMUM_ALLOWED = 0x02000000,
+
+        GENERIC_READ = 0x80000000,
+        GENERIC_WRITE = 0x40000000,
+        GENERIC_EXECUTE = 0x20000000,
+        GENERIC_ALL = 0x10000000,
+
+        FILE_READ_DATA = 0x00000001,
+        FILE_WRITE_DATA =  0x00000002,
+        FILE_APPEND_DATA = 0x00000004,
+        FILE_READ_EA = 0x00000008,
+        FILE_WRITE_EA = 0x00000010,
+        FILE_EXECUTE = 0x00000020,
+        FILE_DELETE_CHILD = 0x00000040,
+        FILE_READ_ATTRIBUTES = 0x00000080,
+        FILE_WRITE_ATTRIBUTES = 0x00000100,
+        FILE_ALL_ACCESS = STANDARD_RIGHTS_REQUIRED | SYNCHRONIZE |  0x000001FF,
+        FILE_GENERIC_READ = STANDARD_RIGHTS_READ | FILE_READ_DATA | FILE_READ_ATTRIBUTES | FILE_READ_EA | SYNCHRONIZE,
+        FILE_GENERIC_WRITE = STANDARD_RIGHTS_WRITE | FILE_WRITE_DATA | FILE_WRITE_ATTRIBUTES | FILE_WRITE_EA | FILE_APPEND_DATA | SYNCHRONIZE,
+        FILE_GENERIC_EXECUTE = STANDARD_RIGHTS_EXECUTE | FILE_READ_ATTRIBUTES | FILE_EXECUTE | SYNCHRONIZE
+    }
+
+    [DllImport("kernel32.dll")]
+    public static extern int GetLastError();
+
+    [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true, PreserveSig = true)]
+    public static extern bool ConvertStringSidToSid(string StringSid, out IntPtr pSid);
+
+    [DllImport("advapi32.dll", EntryPoint = "BuildTrusteeWithSid", CharSet = CharSet.Auto)]
+    static extern void BuildTrusteeWithSid(IntPtr pTrustee, IntPtr pSID);
+
+    [DllImport("advapi32.dll", EntryPoint = "BuildTrusteeWithName", CharSet = CharSet.Auto)]
+    static extern void BuildTrusteeWithName(ref TRUSTEE trustee, string pName);
+
+    [DllImport("advapi32.dll", CharSet = CharSet.Auto)]
+    public static extern uint GetNamedSecurityInfo(string pObjectName, SE_OBJECT_TYPE ObjectType, SECURITY_INFORMATION SecurityInfo, out IntPtr pSidOwner, out IntPtr pSidGroup, out IntPtr pDacl, out IntPtr pSacl, out IntPtr pSecurityDescriptor);
+
+    [DllImport("advapi32.dll", SetLastError = true)]
+    public static extern uint GetEffectiveRightsFromAcl(IntPtr pDacl, ref TRUSTEE pTrustee, ref int pAccessRights);
+
+    [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    static extern bool LookupAccountName(string lpSystemName, string lpAccountName, IntPtr Sid, ref uint cbSid, StringBuilder ReferencedDomainName, ref uint cchReferencedDomainName, out SID_NAME_USE peUse);
+
+    [DllImport("advapi32", CharSet = CharSet.Auto, SetLastError = true)]
+    static extern bool ConvertSidToStringSid(IntPtr pSID, out IntPtr ptrSid);
+
+    public static int GetFileEffectiveRights(string path, string accountName)
+    {
+        return GetEffectiveRights(SE_OBJECT_TYPE.SE_FILE_OBJECT, path, accountName);
+    }
+
+    public static int GetRegKeyEffectiveRights(string path, string accountName)
+    {
+        return GetEffectiveRights(SE_OBJECT_TYPE.SE_REGISTRY_KEY, path, accountName);
+    }
+
+    public static string getSid(string accountName)
+    {
+        string sidString = "";
+        IntPtr pSid = IntPtr.Zero;
+        uint cbSid = 0;
+        StringBuilder referencedDomainName = new StringBuilder();
+        uint cchReferencedDomainName = (uint)referencedDomainName.Capacity;
+        SID_NAME_USE sidUse;
+
+        if (!LookupAccountName(null, accountName, pSid, ref cbSid, referencedDomainName, ref cchReferencedDomainName, out sidUse))
+        {
+                referencedDomainName.EnsureCapacity((int)cchReferencedDomainName);
+                pSid = Marshal.AllocHGlobal((int)cbSid);
+                if (LookupAccountName(null, accountName, pSid, ref cbSid, referencedDomainName, ref cchReferencedDomainName, out sidUse))
+                {
+                    IntPtr ptrSid;
+                    if (ConvertSidToStringSid(pSid, out ptrSid))
+                    {
+                        sidString = Marshal.PtrToStringAuto(ptrSid);
+                        Marshal.Release(ptrSid);
+                    }
+                    else
+                    {
+                        int errorCode = GetLastError();
+                        getWin32ErrorMessage(errorCode);
+                    }
+                }
+            
+        }
+        return sidString;
+    }
+
+    public static IntPtr getTrustee(string accountName)
+    {
+        IntPtr pSid = IntPtr.Zero;
+        uint cbSid = 0;
+        StringBuilder referencedDomainName = new StringBuilder();
+        uint cchReferencedDomainName = (uint)referencedDomainName.Capacity;
+        SID_NAME_USE sidUse;
+
+        if (!LookupAccountName(null, accountName, pSid, ref cbSid, referencedDomainName, ref cchReferencedDomainName, out sidUse))
+        {
+            referencedDomainName.EnsureCapacity((int)cchReferencedDomainName);
+            pSid = Marshal.AllocHGlobal((int)cbSid);
+            if (!LookupAccountName(null, accountName, pSid, ref cbSid, referencedDomainName, ref cchReferencedDomainName, out sidUse))
+            {
+                int errorCode = GetLastError();
+                getWin32ErrorMessage(errorCode);
+            }
+        }
+        return pSid;
+    }
+
+    static int GetEffectiveRights(SE_OBJECT_TYPE type, String path, String accountName)
+    {
+        int mask = 0;
+        IntPtr pOwner = IntPtr.Zero; // pSID
+        IntPtr pGroup = IntPtr.Zero; // pSID
+        IntPtr pSacl = IntPtr.Zero;
+        IntPtr pDacl;// = IntPtr.Zero;
+        IntPtr pSD;// = IntPtr.Zero; // pSECURITY_DESCRIPTOR
+        if (GetNamedSecurityInfo(path, type, SECURITY_INFORMATION.DACL_SECURITY_INFORMATION, out pOwner, out pGroup, out pDacl, out pSacl, out pSD) == 0) {
+            IntPtr pSid = getTrustee(accountName);
+            
+            IntPtr pTrustee = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(TRUSTEE)));
+            BuildTrusteeWithSid(pTrustee, pSid);
+            TRUSTEE trustee = (TRUSTEE)Marshal.PtrToStructure(pTrustee, typeof(TRUSTEE));
+            string tstr = Marshal.PtrToStringAuto(trustee.ptstrName);
+            Marshal.Release(pSid);
+            GetEffectiveRightsFromAcl(pDacl, ref trustee, ref mask);
+        }
+        else 
+        {
+            getWin32ErrorMessage(GetLastError());
+        }
+        return mask;
+    }
+
+    private static string getWin32ErrorMessage(int error)
+    {
+        string errorMessage = new System.ComponentModel.Win32Exception(error).Message;
+        Console.WriteLine(errorMessage);
+        return errorMessage;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace GetFileEffectiveACL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: GetFileEffectiveACL filename");
+                return;
+            }
+            string filename = args[0];
+            WindowsPrincipal principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            Console.WriteLine("WindowsPrincipal: {0}", principal.Identity.Name);
+            Console.WriteLine("hasPermission: {0}\n", isInRole(filename, principal));
+            getPrincipalInfo(principal);
+            getFileSecurity(filename);
+            EffectiveRights.ACCESS_MASK mask = (EffectiveRights.ACCESS_MASK)EffectiveRights.GetFileEffectiveRights(filename, principal.Identity.Name);
+            Console.WriteLine("Access mask: {0:X}", mask);
+            if ((mask & EffectiveRights.ACCESS_MASK.FILE_ALL_ACCESS) == EffectiveRights.ACCESS_MASK.FILE_ALL_ACCESS)
+                Console.WriteLine("Full access");
+            else if ((mask & EffectiveRights.ACCESS_MASK.FILE_GENERIC_READ) == EffectiveRights.ACCESS_MASK.FILE_GENERIC_READ)
+                Console.WriteLine("Read");
+            else if ((mask & EffectiveRights.ACCESS_MASK.FILE_GENERIC_WRITE) == EffectiveRights.ACCESS_MASK.FILE_GENERIC_WRITE)
+                Console.WriteLine("Write");
+            else if ((mask & EffectiveRights.ACCESS_MASK.FILE_GENERIC_EXECUTE) == EffectiveRights.ACCESS_MASK.FILE_GENERIC_EXECUTE)
+                Console.WriteLine("Execute");
+            
+        }
+
+
+        private static void getPrincipalInfo(WindowsPrincipal principal)
+        {
+            Console.WriteLine("WindowsPrincipal: {0}", principal.Identity.Name);
+            Array wbirFields = Enum.GetValues(typeof(WindowsBuiltInRole));
+            foreach (object roleName in wbirFields)
+            {
+                try
+                {
+                    // Cast the role name to a RID represented by the WindowsBuildInRole value.
+                    Console.WriteLine("{0} ({1}) => {2}.", roleName, ((int)roleName).ToString(),
+                        principal.IsInRole((WindowsBuiltInRole)roleName));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("{0}: Could not obtain role for this RID.", roleName);
+                }
+            }
+            // Get the role using the string value of the role.
+            Console.WriteLine("{0} => {1}.", "Administrators",
+                principal.IsInRole("BUILTIN\\" + "Administrators"));
+            Console.WriteLine("{0} => {1}.", "Users",
+                principal.IsInRole("BUILTIN\\" + "Users"));
+            // Get the role using the WindowsBuiltInRole enumeration value.
+            Console.WriteLine("{0} => {1}.", WindowsBuiltInRole.Administrator,
+               principal.IsInRole(WindowsBuiltInRole.Administrator));
+            // Get the role using the WellKnownSidType.
+            SecurityIdentifier sidAdmins = new SecurityIdentifier(WellKnownSidType.BuiltinAdministratorsSid, null);
+            Console.WriteLine("Administrators  {0} => {1}", sidAdmins.Value, principal.IsInRole(sidAdmins));
+            SecurityIdentifier sidUsers = new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null);
+            Console.WriteLine("Users  {0} => {1}.\n", sidUsers.Value, principal.IsInRole(sidUsers));
+        }
+
+        private static void getFileSecurity(string filename) {
+            FileInfo info = new FileInfo(filename);
+            FileSecurity security = info.GetAccessControl();
+            ShowFileSecurity(security);
+        }
+
+        private static bool isInRole(string filename, WindowsPrincipal principal)
+        {
+            // Gets the ACL for the file
+            FileSecurity acl = File.GetAccessControl(filename, AccessControlSections.Access);
+            // Gets the security identifiers of the access rules
+            SecurityIdentifier[] identifiers = acl.GetAccessRules(true, true, typeof(SecurityIdentifier)).OfType<AuthorizationRule>().Select(rule => rule.IdentityReference).Cast<SecurityIdentifier>().ToArray();
+            // Check if the user match any role
+            bool hasPermission = identifiers.Any(identifier => principal.IsInRole(identifier));
+            return hasPermission;
+        }
+        
+        private static void ShowFileSecurity(FileSecurity security)
+        {
+            AuthorizationRuleCollection coll =
+                security.GetAccessRules(true, true, typeof(NTAccount));
+            foreach (FileSystemAccessRule rule in coll)
+            {
+                Console.WriteLine("IdentityReference: {0}",rule.IdentityReference);
+
+                SecurityIdentifier sid = (SecurityIdentifier)rule.IdentityReference.Translate(typeof(SecurityIdentifier));
+                if (null != sid.AccountDomainSid)
+                {
+                    Console.WriteLine("AccountDomainSid: {0}", sid.AccountDomainSid.Value);
+                }
+                Console.WriteLine("Sid: {0}", sid.Value);
+                Console.WriteLine("Access control type: {0}", rule.AccessControlType);
+                Console.WriteLine("Rights: {0}", rule.FileSystemRights);
+                Console.WriteLine("Inherited? {0}", rule.IsInherited);
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
